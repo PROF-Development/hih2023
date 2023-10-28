8a70e9dc817bcb580d623b8e3c9aa175bcb66cee
Kulikov Dmitrii <fle4a555@gmail.com>
2023-10-28 20:07:15 +0300

    PROF-12 Добавлена страница логина

--- front/src/assets/loginUser.js
+++ front/src/assets/loginUser.js
@@ -1,26 +1,13 @@
 import axios from 'axios';
-
-const loginUser = ({username, password}) => {
-    body = {
-        username: username,
-        password: password,
-    }
-    axios.post({
+const loginUser = (form) => {
+    return axios.post({
         url: 'localhost:8000/auth/login',
-        data: body,
+        data: form,
     }).then(response => {
         localStorage.setItem('access_token', response.data.access_token)
-        return {
-            status: 200,
-            message: 'ok',
-            role: response.data.role
-        }
-    }).catch(response => {
-        return {
-            status: 400,
-            message: 'Неверный логин и/или пароль',
-            role: null,
-        }
+        return response.data.role
+    }).catch(error => {
+        return null
     })
 }
 
