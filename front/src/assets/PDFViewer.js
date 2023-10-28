39303338b44d51e36c3875c414bbe10814a00ce6
roman0309 <romanmalov44@yandex.ru>
2023-10-28 12:09:24 +0300

    PROF-8 PDFViewer

---
+++ front/src/assets/PDFViewer.js
@@ -0,0 +1,16 @@
+import { Viewer, Worker } from '@react-pdf-viewer/core';
+import '@react-pdf-viewer/core/lib/styles/index.css';
+
+
+
+const PDFViewer = () => {
+    return (
+        <div>
+            <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+                    <Viewer fileUrl=".your_path.pdf" />
+            </Worker>
+        </div>
+    );
+};
+
+export default PDFViewer;
