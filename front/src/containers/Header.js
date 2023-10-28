f4f00e883a88baaed0aa50fa36addf71e9907658
roman0309 <romanmalov44@yandex.ru>
2023-10-28 16:38:20 +0300

    PROF-12 Header + стили

--- front/src/containers/Header.js
+++ front/src/containers/Header.js
@@ -2,9 +2,23 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-
+        <header class="header">
+        <div class="container">
+            <div class="header-wrapper">    
+                <div class="row align-items-center justify-content-between">
+                    <div class="col-12 col-sm-4 col-md-4 col-lg-2">
+                        <a href="index.html" class="header-logo"><img src="./img/big-logo.png" alt="logo"></img></a>
+                    </div>
+                    <div class="col-12 col-sm-8 col-md-6">
+                        <div class="header-buttons">
+                            <Link to="#" class="header-buttons__btn btn btn-trans">Поиск документации</Link>
+                            <Link to="#" class="header-buttons__btn btn btn-primary">Вход</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+    </header>
     )
 }
 
