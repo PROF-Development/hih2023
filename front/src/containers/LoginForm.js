b71afb89f156c2f6e43da75e8c3f4e888c35f3e6
roman0309 <romanmalov44@yandex.ru>
2023-10-28 18:17:06 +0300

    PROF-12 Добавлена компонента LoginForm

--- front/src/containers/LoginForm.js
+++ front/src/containers/LoginForm.js
@@ -2,6 +2,10 @@ import React,{useState} from "react";
 import { useNavigate } from "react-router-dom";
 import { useStore } from "react-redux";
 import { superUserAction, setAuthAction } from "../store/actions/auth";
+import  '../styles/reset.css';
+import '../styles/bootstrap-grid.min.css';
+import '../styles/style.css';
+
 const LoginForm = () => {
     const store = useStore()
     const navigate = useNavigate()
@@ -21,7 +25,33 @@ const LoginForm = () => {
         navigate('/')
     }
     return (
-        <div></div>
+        <section className="section auth">
+            <div className="container">
+                <div className="auth-wrapper">
+                    <div className="row align-items-center">
+                        <div className="col-12 col-md-4 col-lg-6">
+                            <div className="auth-img">
+                                <img src="img/big-logo.png" alt="logo"></img>
+                            </div>
+                        </div>
+                        <div className="col-12 col-md-8 col-lg-6">
+                            <form method="POST" className="auth-form">
+                                <h2 className="auth-form__title">Войти</h2>
+                                <div className="auth-form__group form-group">
+                                    <label className="auth-form__label" for="login">Логин</label>
+                                    <input className="auth-form__input" id="login" name="login" type="text" required></input>
+                                </div>
+                                <div className="auth-form__group form-group">
+                                    <label className="auth-form__label" for="password">Введите пароль</label>
+                                    <input className="auth-form__input" id="password" name="password" type="password" required></input>
+                                </div>
+                                <button type="submit" className="btn btn-primary auth-form__btn">Войти</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
