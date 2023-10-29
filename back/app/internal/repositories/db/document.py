39e3782006fccf60883e15832dc3e7ebc9d53eec
Amer-On <a20051772@yandex.ru>
2023-10-29 09:17:33 +0300

    PROF-9 Добавлена возможность добавления типов документов, документов и просмотра

--- back/app/internal/repositories/db/document.py
+++ back/app/internal/repositories/db/document.py
@@ -2,7 +2,7 @@ import datetime
 from uuid import UUID
 
 from internal.repositories.db.base import BaseRepository
-from internal.repositories.db.models import Document, DocumentTags
+from internal.repositories.db.models import Document, DocumentTags, DocumentTypes
 from sqlalchemy import delete, insert, select, update
 
 
@@ -10,9 +10,22 @@ class DocumentRepository(BaseRepository):
     def __init__(self):
         super().__init__()
         self.table = Document
-        self.tags_table = DocumentTags
+        self.doc_types = DocumentTypes
 
-    def get_document(self, document_uuid: UUID):
+    async def get_document_types(self):
+        query = select(self.doc_types)
+
+        async with self.session_maker() as session:
+            res = await session.execute(query)
+        return res.all()
+
+    async def add_document_type(self, document_type: str):
+        query = insert(self.doc_types).values(type=document_type)
+        async with self.session_maker() as session:
+            await session.execute(query)
+            await session.commit()
+
+    async def get_document(self, document_uuid: UUID):
         query = select(self.table).where(self.table.id == document_uuid)
 
         async with self.session_maker() as session:
@@ -20,7 +33,7 @@ class DocumentRepository(BaseRepository):
 
         return res.first()
 
-    def get_documents(
+    async def get_documents(
         self, document_type: str = None, name: str = None, number: str = None, release_date: datetime.date = None, start_date: datetime.date = None
     ):
         query = select(self.table)
@@ -45,7 +58,7 @@ class DocumentRepository(BaseRepository):
 
         return res.all()
 
-    def add_document(self, document_uuid: UUID, document_type: str, name: str, number: str, release_date: datetime.date, start_date: datetime.date):
+    async def add_document(self, document_uuid: UUID, document_type: str, name: str, number: str, release_date: datetime.date, start_date: datetime.date):
         query = insert(self.table).values(
             id=document_uuid, document_type=document_type, name=name, number=number, release_date=release_date, start_date=start_date
         )
@@ -84,7 +97,7 @@ class DocumentRepository(BaseRepository):
             await session.execute(query)
             await session.commit()
 
-    async def delete_document_access(self, document_uuid: UUID):
+    async def delete_document(self, document_uuid: UUID):
         query = delete(self.table).where(self.table.id == document_uuid)
 
         async with self.session_maker() as session:
